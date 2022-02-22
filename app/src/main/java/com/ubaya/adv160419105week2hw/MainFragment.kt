b6c4c1ed3a44f7820c045762acc183c72405fba2
GILBERT <s160419105@student.ubaya.ac.id>
2022-02-22 21:37:12 +0700

    Selesai

--- app/src/main/java/com/ubaya/adv160419105week2hw/MainFragment.kt
+++ app/src/main/java/com/ubaya/adv160419105week2hw/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +15,30 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var num1 = (0..100).random()
+        var num2 = (0..100).random()
+        var score = 0
+        textSoal.text = "$num1 + $num2"
+        textTemp.text = "Your Score : $score"
+        buttonSubmit.setOnClickListener {
+            var answer = textInputAnswer.editText?.text.toString()
+            if(answer != null){
+                if(num1 + num2 == answer.toInt()){
+                    num1 = (0..100).random()
+                    num2 = (0..100).random()
+                    textSoal.text = "$num1 + $num2"
+                    score = score + 1
+                    textTemp.text = "Your Score : $score"
+                }
+                else{
+                    var action = MainFragmentDirections.actionMainFragmentToResultFragment(score)
+                    Navigation.findNavController(it).navigate(action)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/ubaya/adv160419105week2hw/ResultFragment.kt
+++ app/src/main/java/com/ubaya/adv160419105week2hw/ResultFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_result.*
 
 class ResultFragment : Fragment() {
 
@@ -14,5 +16,17 @@ class ResultFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let{
+            var score = ResultFragmentArgs.fromBundle(requireArguments()).score
+            textScore.text = "Your Final Score is $score"
+        }
+        buttonBack.setOnClickListener {
+            val action = ResultFragmentDirections.actionResultFragmentToMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
